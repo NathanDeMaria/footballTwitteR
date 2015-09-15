e08e73542f623a4d13c463489475c6b648c90a12
NathanDeMaria <nathandmaria@gmail.com>
2015-09-15 12:49:05 -0500

    Search back a week, log betterish

--- search.R
+++ search.R
@@ -4,22 +4,27 @@ source('sqlite.R')
 search_string <- commandArgs(trailingOnly = T)[1]
 
 today <- Sys.Date()
-two_days <- as.character(today - 2)
+in_the_past <- as.character(today - 8)
 yesterday <- as.character(today - 1)
 
-tweets <- suppressWarnings(searchTwitter(search_string, n = 1000, since = two_days, until = yesterday))
+tweets <- suppressWarnings(searchTwitter(search_string, n = 1000, since = in_the_past, until = yesterday))
 tweet_df <- twListToDF(tweets)
 
 tweet_df$search_string <- search_string
+tweet_df$scrape_time <- Sys.time()
 
 sqlite <- create_sqlite_accessor('tweets.db')
 
-log_base <- paste0('%s <', search_string, '> %s')
+log_base <- paste0('%s <', search_string, '> %s\n')
+
+collected <- nrow(tweet_df)
+cat(sprintf(log_base, Sys.time(), sprintf("Collected %d tweets", collected)))
+
 if (!file.exists('tweets.db')) {
-  cat(sprintf(log_base, Sys.time(), sprintf('Creating new table with %d tweets', nrow(tweet_df))))
   sqlite$create_table(tweet_df, 'tweets', id = 'id')
-  
+  saved <- collected
 } else {
-  cat(sprintf(log_base, Sys.time(), sprintf('Appending %d tweets to table', nrow(tweet_df))))
-  sqlite$append_table(tweet_df, 'tweets')
+  saved <- sqlite$append_table(tweet_df, 'tweets')
 }
+
+cat(sprintf(log_base, Sys.time(), sprintf("Saved %d new tweets", saved)))
