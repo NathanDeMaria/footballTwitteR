19e9f4ceb66a4013de92efb06b9ba491e21a6990
NathanDeMaria <nathandmaria@gmail.com>
2015-09-22 19:14:55 -0500

    Silence the registering script

--- register_oauth.R
+++ register_oauth.R
@@ -2,7 +2,7 @@ library(twitteR)
 
 source('config/app_config.R')
 
-setup_twitter_oauth(get_config('consumer_key'), 
+capture.output(setup_twitter_oauth(get_config('consumer_key'), 
                     get_config('consumer_secret'), 
                     get_config('access_token'), 
-                    get_config('access_token_secret'))
+                    get_config('access_token_secret')))
