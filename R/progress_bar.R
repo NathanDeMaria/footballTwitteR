71dbf103a83a23eac52520861c0d07aabff51add
NathanDeMaria <nathandmaria@gmail.com>
2015-12-24 13:17:14 -0600

    Add estimated time to progress bar

--- R/progress_bar.R
+++ R/progress_bar.R
@@ -3,12 +3,21 @@ progress_bar <- function(n, title) {
   progress <- 0
   
   tryCatch({
-    pb <- winProgressBar(title = title, max = n, label = "0% completed")
+    pb <- winProgressBar(title = title, max = n, label = "Initializing...")
+    
+    start_time <- Sys.time()
     
     inc <- function() {
       progress <<- progress + 1
+      current_time <- Sys.time()
+      
+      current_completion_rate <- progress / as.numeric(current_time - start_time, units = 'secs')
+      time_remaining <- (n - progress) * current_completion_rate
+      
+      remaining_string <- sprintf("%.0f seconds remaining", time_remaining)
+      
       completion <- sprintf("%.02f%% completed", progress / n * 100)
-      setWinProgressBar(pb, progress, label = completion)
+      setWinProgressBar(pb, progress, title = sprintf("%s (%s)", title, completion), label = remaining_string)
     }
     
     close_bar <- function() {
