6c4a0b3f313b591394e753a37cb030ad65074217
NathanDeMaria <nathandmaria@gmail.com>
2015-12-23 23:09:51 -0600

    This fails sometimes too :/

--- R/get_images.R
+++ R/get_images.R
@@ -77,7 +77,15 @@ save_images <- function(sample_date) {
   
   apply(tweet_images, 1, function(r) {
     if (!file.exists(r['filename'])) {
-      download.file(r['image'], destfile = r['filename']) 
+      tryCatch({
+        download.file(r['image'], destfile = r['filename'])   
+      }, error = function(e) {
+        if(substr(e$message, 0, 15) == "cannot open URL") {
+          warning(sprintf("Cannot open URL %s", r['image']))
+        } else {
+          stop(e$message)
+        }
+      })
     }
   })
   tweet_images
